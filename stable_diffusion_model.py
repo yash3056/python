6c23e68b0725b5d469599ee6633fdf5d9856685a
yash3056 <yashnarayanp8@gmail.com>
2024-04-12 16:45:07 +0530

    update

--- stable_diffusion_model.py
+++ stable_diffusion_model.py
@@ -9,7 +9,7 @@ torch.cuda.is_available = lambda: False
 
 pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float32)
 
-prompt = "a photo of an astronaut riding a horse on mars"
+prompt = "a photo of a man on the moon"
 image = pipe(prompt).images[0]
 
-image.save("astronaut_rides_horse.png")
+image.save("man_on_moon.png")
